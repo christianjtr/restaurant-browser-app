b1c9cfc9ccf15efa4779367204c54e31361e422e
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-11 04:30:01 +0200

    Add tailwind settings along with vite bundler settings (development)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ['"Poppins"', 'serif'],
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: ['light'],
-},
-}
-
+  },
+};
