f709f47e3b508f9856a64998bfd96bf6613721e6
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-11 17:19:39 +0200

    Add path alias to the project

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: require('tailwindcss'),
-    autoprefixer: require('autoprefixer'),
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,16 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+  },
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: resolve(__dirname, './src/assets') },
+      { find: '@components', replacement: resolve(__dirname, './src/components') },
+      { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
+      { find: '@pages', replacement: resolve(__dirname, './src/pages') },
+      { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+      { find: '@services', replacement: resolve(__dirname, './src/services') },
+      { find: '@app-types', replacement: resolve(__dirname, './src/types') },
+    ],
   },
 });
