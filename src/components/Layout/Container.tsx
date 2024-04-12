78f85fdbe56a27847370f5d7aed900ae7282ce84
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-12 04:08:21 +0200

    Enhance container component

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 export interface ContainerProps {
   children: React.ReactElement;
 }
@@ -7,7 +6,7 @@ export interface ContainerProps {
 export const Container = ({ children }: ContainerProps): React.ReactElement => {
   return (
     <main>
-      <div className="sm:w-1/2 md:container mx-auto bg-gray-300 px-6 h-full">{children}</div>
+      <div className="sm:w-1/2 md:container mx-auto px-6 h-fit">{children}</div>
     </main>
   );
 };
