f210346e34bd275817f3db56a003bf79f21c9af5
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-12 04:56:46 +0200

    Fix responsiveness and place LoaderSpinner component file in another folder

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -6,7 +6,7 @@ export interface ContainerProps {
 export const Container = ({ children }: ContainerProps): React.ReactElement => {
   return (
     <main>
-      <div className="sm:w-1/2 md:container mx-auto px-6 h-fit">{children}</div>
+      <div className="sm:w-full md:w-1/2 mx-auto px-6 h-fit">{children}</div>
     </main>
   );
 };
