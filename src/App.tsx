45a213ae263ee52d1520198796a8fe659e09e542
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-14 19:47:28 +0200

    Set app environment base path up

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { Header, Container, LoaderSpinner } from './components/Layout';
 import { AppRoutes } from './routes/AppRoutes';
 import { PurchaseProvider } from './contexts/Purchase/PurchaseProvider';
 import { GeolocationProvider } from './contexts/Geolocation/GeolocationProvider';
+import { APP_BASE_PATH } from './config/app.config';
 import './App.css';
 
 const App = (): React.ReactElement => {
   return (
     <PurchaseProvider>
       <GeolocationProvider>
-        <Router basename={import.meta.env.VITE_APP_BASE_PATH}>
+        <Router basename={APP_BASE_PATH}>
           <Header />
           <Container>
             <React.Suspense fallback={<LoaderSpinner />}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  resolve: {
-    alias: [
-      { find: '@adapters', replacement: resolve(__dirname, './src/adapters') },
-      { find: '@assets', replacement: resolve(__dirname, './src/assets') },
-      { find: '@components', replacement: resolve(__dirname, './src/components') },
-      { find: '@contexts', replacement: resolve(__dirname, './src/contexts') },
-      { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
-      { find: '@pages', replacement: resolve(__dirname, './src/pages') },
-      { find: '@routes', replacement: resolve(__dirname, './src/routes') },
-      { find: '@services', replacement: resolve(__dirname, './src/services') },
-      { find: '@app-types', replacement: resolve(__dirname, './src/types') },
-      { find: '@utils', replacement: resolve(__dirname, './src/utils') },
-    ],
-  },
-});
+
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [svgr(), react()],
+    server: {
+      host: true,
+      port: 3000,
+    },
+    base: process.env.VITE_APP_BASE_PATH,
+    resolve: {
+      alias: [
+        { find: '@adapters', replacement: resolve(__dirname, './src/adapters') },
+        { find: '@assets', replacement: resolve(__dirname, './src/assets') },
+        { find: '@components', replacement: resolve(__dirname, './src/components') },
+        { find: '@config', replacement: resolve(__dirname, './src/config') },
+        { find: '@contexts', replacement: resolve(__dirname, './src/contexts') },
+        { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
+        { find: '@pages', replacement: resolve(__dirname, './src/pages') },
+        { find: '@routes', replacement: resolve(__dirname, './src/routes') },
+        { find: '@services', replacement: resolve(__dirname, './src/services') },
+        { find: '@app-types', replacement: resolve(__dirname, './src/types') },
+        { find: '@utils', replacement: resolve(__dirname, './src/utils') },
+      ],
+    },
+  });
+};
